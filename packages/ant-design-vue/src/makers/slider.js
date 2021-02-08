c04d3bd33b2568f00d2a4d8c9959ce76ed71c6c3
shenwenjie <15805199474@163.com>
2021-02-08 10:31:46 +0800

    精简

--- packages/ant-design-vue/src/makers/slider.js
+++
@@ -1,7 +0,0 @@
-import {creatorTypeFactory} from '@xl-form-create/core';
-
-const name = 'slider';
-
-export default {
-    sliderRange: creatorTypeFactory(name, true, 'range')
-};
--- packages/iview4/src/makers/timePicker.js
+++
@@ -1,8 +0,0 @@
-import {creatorTypeFactory} from '@xl-form-create/core';
-
-const name = 'timePicker';
-
-export default {
-    time: creatorTypeFactory(name, 'time'),
-    timeRange: creatorTypeFactory(name, 'timerange')
-};
